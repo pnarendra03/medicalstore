421d31fa8e820453145b83b06ef2b3408bb4dbd0
Vanshika-kejriwal <kejriwalvanshika@gmail.com>
2022-10-04 19:02:00 +0530

    project created

---
+++ src/test/java/com/medicalstore/medicalstore/MedicalstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicalstore.medicalstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
