30097d8768d7755ecc2c2c0e5df8014cd94709b8
Vanshika-kejriwal <kejriwalvanshika@gmail.com>
2022-10-30 16:35:07 +0530

    templates integrated

--- src/main/java/com/medicalstore/medicalstore/controllers/HomeController.java
+++ src/main/java/com/medicalstore/medicalstore/controllers/HomeController.java
@@ -9,5 +9,33 @@ public class HomeController {
     public String home(){
         return "index";
     }
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/profile")
+    public String profile(){
+        return "profile";
+    }
+    @GetMapping("/empty_cart")
+    public String empty_cart(){
+        return "emptycart";
+    }
+    @GetMapping("/orders")
+    public String orders(){
+        return "orders";
+    }
+    @GetMapping("/product_detail")
+    public String product_detail(){
+        return "productdetail";
+    }
+    @GetMapping("/signup")
+    public String signup(){
+        return "signup";
+    }
+    @GetMapping("/profile/editaddress")
+    public String edit_address(){
+        return "editaddress";
+    }
 }
 
