6c0dabbbdad050d26de7a531f636e8537cf142fd
hemanshi sarwalia <hemanshiofficial04@gmail.com>
2022-11-02 11:26:53 +0530

    signup logic

--- src/main/java/com/medicalstore/medicalstore/controllers/HomeController.java
+++ src/main/java/com/medicalstore/medicalstore/controllers/HomeController.java
@@ -1,10 +1,17 @@
 package com.medicalstore.medicalstore.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.medicalstore.medicalstore.models.Customer;
 @Controller
 public class HomeController {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @GetMapping("/")
     public String home(){
         return "index";
@@ -33,10 +40,37 @@ public class HomeController {
     public String signup(){
         return "signup";
     }
+    @PostMapping("/signup")
+    public String signup(Customer customer){
+        String insert_query ="insert into Customer("
+                +"idcustomer, username, c_sex, c_phn_no, c_aadhar_no, c_state, c_street, c_city, c_pincode, c_house_no)"
+                + "value"
+                + "(?,?,?,?,?,?,?,?,?,?)";
+
+        int rows= jdbcTemplate.update(insert_query, 
+                            customer.getidcustomer(),
+                            customer.getusername(),
+                            customer.getc_sex(),
+                            customer.getc_ph_no(),
+                            customer.getc_aadhar_no(),
+                            customer.getc_state(),
+                            customer.getc_street(),
+                            customer.getc_city(),
+                            customer.getc_pincode(),
+                            customer.getc_house_no());
+
+        if(rows ==1) {
+            return "success";//create success html page
+        }else{
+            return "error";//create error html page
+        }
+    }
+
     @GetMapping("/forgotpassword/")
     public String forgot_password(){
         return "forgotpassword";
     }
+
     @GetMapping("/profile/editaddress")
     public String edit_address(){
         return "editaddress";
--- src/main/java/com/medicalstore/medicalstore/models/Customer.java
+++ src/main/java/com/medicalstore/medicalstore/models/Customer.java
@@ -3,73 +3,73 @@ package com.medicalstore.medicalstore.models;
 public class Customer {
     private int idcustomer;
     private String username;
-    private String customer_sex;
-    private String customer_ph_no;
-    private String customer_aadhar_no;
-    private String customer_state;
-    private String customer_street;
-    private String customer_city;
-    private int customer_pincode;
-    private String customer_house_no;
-    public int getIdcustomer() {
+    private String c_sex;
+    private String c_ph_no;
+    private String c_aadhar_no;
+    private String c_state;
+    private String c_street;
+    private String c_city;
+    private int c_pincode;
+    private String c_house_no;
+    public int getidcustomer() {
         return idcustomer;
     }
-    public void setIdcustomer(int idcustomer) {
+    public void setidcustomer(int idcustomer) {
         this.idcustomer = idcustomer;
     }
-    public String getUsername() {
+    public String getusername() {
         return username;
     }
-    public void setUsername(String username) {
+    public void setusername(String username) {
         this.username = username;
     }
-    public String getCustomer_sex() {
-        return customer_sex;
+    public String getc_sex() {
+        return c_sex;
     }
-    public void setCustomer_sex(String customer_sex) {
-        this.customer_sex = customer_sex;
+    public void setc_sex(String c_sex) {
+        this.c_sex = c_sex;
     }
-    public String getCustomer_ph_no() {
-        return customer_ph_no;
+    public String getc_ph_no() {
+        return c_ph_no;
     }
-    public void setCustomer_ph_no(String customer_ph_no) {
-        this.customer_ph_no = customer_ph_no;
+    public void setc_ph_no(String c_ph_no) {
+        this.c_ph_no = c_ph_no;
     }
-    public String getCustomer_aadhar_no() {
-        return customer_aadhar_no;
+    public String getc_aadhar_no() {
+        return c_aadhar_no;
     }
-    public void setCustomer_aadhar_no(String customer_aadhar_no) {
-        this.customer_aadhar_no = customer_aadhar_no;
+    public void setc_aadhar_no(String c_aadhar_no) {
+        this.c_aadhar_no = c_aadhar_no;
     }
-    public String getCustomer_state() {
-        return customer_state;
+    public String getc_state() {
+        return c_state;
     }
-    public void setCustomer_state(String customer_state) {
-        this.customer_state = customer_state;
+    public void setc_state(String c_state) {
+        this.c_state = c_state;
     }
-    public String getCustomer_street() {
-        return customer_street;
+    public String getc_street() {
+        return c_street;
     }
-    public void setCustomer_street(String customer_street) {
-        this.customer_street = customer_street;
+    public void setc_street(String c_street) {
+        this.c_street = c_street;
     }
-    public String getCustomer_city() {
-        return customer_city;
+    public String getc_city() {
+        return c_city;
     }
-    public void setCustomer_city(String customer_city) {
-        this.customer_city = customer_city;
+    public void setc_city(String c_city) {
+        this.c_city = c_city;
     }
-    public int getCustomer_pincode() {
-        return customer_pincode;
+    public int getc_pincode() {
+        return c_pincode;
     }
-    public void setCustomer_pincode(int customer_pincode) {
-        this.customer_pincode = customer_pincode;
+    public void setc_pincode(int c_pincode) {
+        this.c_pincode = c_pincode;
     }
-    public String getCustomer_house_no() {
-        return customer_house_no;
+    public String getc_house_no() {
+        return c_house_no;
     }
-    public void setCustomer_house_no(String customer_house_no) {
-        this.customer_house_no = customer_house_no;
+    public void setc_house_no(String c_house_no) {
+        this.c_house_no = c_house_no;
     }
     
 }
